bced55c84e0a1d9491da299c80ec7c1b127c1848
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-12-05 09:28:23 +1100

    rest configuration changed

--- src/main/kotlin/com/okta/kotlin/RestConfiguration.kt
+++ src/main/kotlin/com/okta/kotlin/RestConfiguration.kt
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
 
-// Extends the rest config and exposes the idea for the coffee shop model class and updates the base path
 @Configuration
-open class RestConfig : RepositoryRestConfigurer {
+open class RestConfiguration : RepositoryRestConfigurer {
     override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration?) {
         config?.exposeIdsFor(CoffeeShopModel::class.java)
-        config?.setBasePath("/api")
+        config?.setBasePath("/api");
     }
 }
